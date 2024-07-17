23d3cf8817bb4ee443c2033c74952fef8e505d60
bulendsaciragic <bulendsaciragic@outlook.com>
2024-07-17 07:52:26 +0200

    Add UI

--- script.js
+++ script.js
@@ -1,94 +1,116 @@
-let roundCount = 1;
+let roundCount = 0;
+let humanScore = 0;
+let computerScore = 0; 
+
+const rockButton = document.querySelector("#rockButton");
+const paperButton = document.querySelector("#paperButton");
+const scissorsButton = document.querySelector("#scissorsButton");
+
+const roundCountSpan = document.querySelector("#roundCountSpan");
+
+const result = document.querySelector("#result");
+result.textContent = `${humanScore}:${computerScore}`
+
+const roundResult = document.createElement("p");
+result.append(roundResult);
+
+roundCountSpan.textContent = `${String(roundCount)}`
+
+const messageDiv = document.querySelector("#messageDiv");
+const message = document.createElement("p");
+messageDiv.append(message);
 
 function getComputerChoice () {
         
-        function getRandomInt() {
-        return Math.floor(Math.random() * 3);
-        }
+        const choices = ["rock", "paper", "scissors"];
+        const randomIndex = Math.floor(Math.random() * choices.length);
+        return choices[randomIndex];
 
-        const RandomInt = getRandomInt();
-        
-        let computerChoice;
-
-        if (RandomInt === 0) {
-            computerChoice = "Rock";
-        } else if (RandomInt === 1) {
-            computerChoice = "Paper";
-        } else if (RandomInt === 2){
-            computerChoice = "Scissors";
-        }
-
-        return computerChoice.toLowerCase();
 }
 
-function getHumanChoice () {
-    let humanChoice = prompt(`Round ${roundCount} / 5 | Rock, Paper, Scissors. What is your choice?`);
-    return humanChoice.toLowerCase(); 
-}
-
-let humanChoice;
 let computerChoice;
 
-function playGame() {
-
-    let humanScore = 0;
-    let computerScore = 0;
-
-    function playRound (humanChoice, computerChoice) {
+    function playRound (humanChoice) {
     
-        humanChoice = getHumanChoice();
         computerChoice = getComputerChoice();
-
+        console.log(`Human choice: ${humanChoice}, Computer choice: ${computerChoice}`);
+    
         if (humanChoice === computerChoice) {
             humanScore = humanScore + 1;
             computerScore = computerScore + 1;
             roundCount = roundCount + 1;
-            alert(`Draw! You said ${humanChoice} and computer said ${computerChoice} too. Result is ${humanScore}:${computerScore}.`);
+            roundCountSpan.textContent = `${String(roundCount)}`
+            result.textContent = `${humanScore}:${computerScore}`
+            message.textContent = `Draw! You said ${humanChoice} and computer said ${computerChoice} too. Result is ${humanScore}:${computerScore}.`;
         } else if (humanChoice === "rock" && computerChoice === "scissors"){
             humanScore = humanScore + 1;
             roundCount = roundCount + 1;
-            alert(`You won! You said ${humanChoice} and computer said ${computerChoice}. Result is ${humanScore}:${computerScore}.`);
+            roundCountSpan.textContent = `${String(roundCount)}`
+            result.textContent = `${humanScore}:${computerScore}`
+            message.textContent = `You won! You said ${humanChoice} and computer said ${computerChoice}. Result is ${humanScore}:${computerScore}.`;
         } else if (humanChoice === "rock" && computerChoice === "paper"){
             computerScore = computerScore + 1;
             roundCount = roundCount + 1;
-            alert(`You lost! You said ${humanChoice} and computer said ${computerChoice}. Result is ${humanScore}:${computerScore}.`);
+            roundCountSpan.textContent = `${String(roundCount)}`
+            result.textContent = `${humanScore}:${computerScore}`
+            message.textContent = `You lost! You said ${humanChoice} and computer said ${computerChoice}. Result is ${humanScore}:${computerScore}.`;
         } else if (humanChoice === "paper" && computerChoice === "rock"){
             humanScore = humanScore + 1;
             roundCount = roundCount + 1;
-            alert(`You won! You said ${humanChoice} and computer said ${computerChoice}. Result is ${humanScore}:${computerScore}.`);
+            roundCountSpan.textContent = `${String(roundCount)}`
+            result.textContent = `${humanScore}:${computerScore}`
+            message.textContent = `You won! You said ${humanChoice} and computer said ${computerChoice}. Result is ${humanScore}:${computerScore}.`;
         } else if (humanChoice === "paper" && computerChoice === "scissors"){
             computerScore = computerScore + 1;
             roundCount = roundCount + 1;
-            alert(`You lost! You said ${humanChoice} and computer said ${computerChoice}. Result is ${humanScore}:${computerScore}.`);
+            roundCountSpan.textContent = `${String(roundCount)}`
+            result.textContent = `${humanScore}:${computerScore}`
+            message.textContent = `You lost! You said ${humanChoice} and computer said ${computerChoice}. Result is ${humanScore}:${computerScore}.`;
         } else if (humanChoice === "scissors" && computerChoice === "rock"){
             computerScore = computerScore + 1;
             roundCount = roundCount + 1;
-            alert(`You lost! You said ${humanChoice} and computer said ${computerChoice}. Result is ${humanScore}:${computerScore}.`);
+            roundCountSpan.textContent = `${String(roundCount)}`
+            result.textContent = `${humanScore}:${computerScore}`
+            message.textContent = `You lost! You said ${humanChoice} and computer said ${computerChoice}. Result is ${humanScore}:${computerScore}.`;
         } else if (humanChoice === "scissors" && computerChoice === "paper"){
             humanScore = humanScore + 1;
             roundCount = roundCount + 1;
-            alert(`You won! You said ${humanChoice} and computer said ${computerChoice}. Result is ${humanScore}:${computerScore}.`);
+            roundCountSpan.textContent = `${String(roundCount)}`
+            result.textContent = `${humanScore}:${computerScore}`
+            message.textContent = `You won! You said ${humanChoice} and computer said ${computerChoice}. Result is ${humanScore}:${computerScore}.`;
         } else {
-            alert("Wrong input! Try again.");
+            message.textContent = "Wrong input! Try again.";
             playRound(humanChoice, computerChoice);
         }
-    
-    }
+        
+        if (roundCount === 5) {
+            if (humanScore > computerScore) {
+                message.textContent = `You won! Final result is ${humanScore}:${computerScore}`;
+            } else if (humanScore < computerScore) {
+                message.textContent = `You lost! Final result is ${humanScore}:${computerScore}`;
+            } else {
+                message.textContent = `It's a draw! Final result is ${humanScore}:${computerScore}`;
+            }
+            setTimeout(resetGame, 3000);
+        }
 
-    playRound(humanChoice, computerChoice);
-    playRound(humanChoice, computerChoice);
-    playRound(humanChoice, computerChoice);
-    playRound(humanChoice, computerChoice);
-    playRound(humanChoice, computerChoice);
+        }
 
-    if (humanScore > computerScore ) {
-        alert(`Congrats! You are the winner with the final result of ${humanScore}:${computerScore}.`)
-    } else if (humanScore < computerScore ) {
-        alert(`Bad luck! You lost with the final result of ${humanScore}:${computerScore}.`)
-    } else {
-        alert(`No winner! The final result is ${humanScore}:${computerScore}.`)
-    }
+        function resetGame() {
+            roundCount = 0;
+            humanScore = 0;
+            computerScore = 0;
+            roundCountSpan.textContent = `${String(roundCount)}`;
+            result.textContent = `${humanScore}:${computerScore}`
+            message.textContent = '';
+        }
 
-}
-
-playGame();
+rockButton.addEventListener("click", () => {
+    playRound("rock");
+});
+paperButton.addEventListener("click", () => {
+    playRound("paper");
+});
+scissorsButton.addEventListener("click", () => {
+    playRound("scissors");
+});
